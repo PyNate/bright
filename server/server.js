f35c22a552df2225f5cc7add7cc00f682746c775
PyNate <n.leonard668@gmail.com>
2016-12-14 17:23:52 -0600

    Configure server for React Router

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import router from './router';
@@ -11,6 +12,11 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use('/', router);
+
+app.get('*', function (req, res) {
+  // and drop 'public' in the middle of here
+  res.sendFile(path.join(path.dirname(__dirname), 'public', 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
